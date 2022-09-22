5372aea1fa04b7185f1451cebf1464b4f92d48be
carlosalesilva <carlosalexandresilva100@gmail.com>
2022-09-22 17:03:09 -0300

    Implementação tela de login e integração com a api.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import * as C from "./styles";
+import { Link} from "react-router-dom";
+import {LoginUser} from "../../service/api";
+
+import { AuthContext } from "../../App"
+import history from "../../history";
 
 const Login = () => {
 
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+  let token = ''
+  
+  const auth = useContext(AuthContext);
+
+  const handleLogin = () => {
+    if (!email | !senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+
+
+    const user = {
+      "email": email,
+      "password": senha
+    }
+    LoginUser(user).then((response)=>{
+      auth.setAuth({token: response.data.token, nome: response.data.nome});
+      history.push("/");
+    }).catch((error)=>{
+        alert("Usario ou senha incorreta");
+    })
+
+  };
+
   return (
-    <div>Login</div>
+    <C.Container>
+      <C.Label>SISTEMA DE LOGIN</C.Label>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Digite seu E-mail"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua Senha"
+          value={senha}
+          onChange={(e) => [setSenha(e.target.value), setError("")]}
+        />
+        <C.labelError>{error}</C.labelError>
+        <Button Text="Entrar" onClick={handleLogin} />
+        <C.LabelSignup>
+          Não tem uma conta?
+          <C.Strong>
+            <Link to="/cadastro_users">&nbsp;Registre-se</Link>
+          </C.Strong>
+        </C.LabelSignup>
+      </C.Content>
+    </C.Container>
   );
 };
 
