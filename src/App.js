364ecf5e89ff219d4a920684d4ff84b3c8fd5876
carlosalesilva <carlosalexandresilva100@gmail.com>
2022-09-22 16:39:45 -0300

    Criação do projeto e implementação da autenticação com context API

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Router, Route, Redirect } from 'react-router-dom';
+import history from './history';
+
+import { createContext, useState } from 'react';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+export const AuthContext = createContext(null);
+
+function App() {
+
+  const [auth, setAuth] = useState({ token: localStorage.getItem("token"), nome: localStorage.getItem("nome")});
+  const setAuthLS = (newAuth)=>{
+    setAuth(newAuth);
+    console.log(newAuth)
+    localStorage.setItem("token", newAuth.token);
+    localStorage.setItem("nome", newAuth.nome);
+  }
+
+  return (
+    <AuthContext.Provider value={{ token: auth, setAuth: setAuthLS }}>
+      <Router history={history}>
+        <Route exact path="/">
+          {
+            auth.token == null ?
+              <Redirect to="/login"></Redirect> :
+              <Home></Home>
+          }
+
+        </Route>
+        <Route exact path="/login" component={Login}>
+        </Route>
+      </Router>
+    </AuthContext.Provider>
+
+  )
+}
+
+export default App;
